da5c9a63d07319ba27e41835c85f8e0d2adff79b
Piers Kwan <me@pierskwan.com>
2021-10-24 20:28:56 -0400

    Added flag manipulation functions and clock enable to the dma driver

--- src/stm32f3hal/dma.c
+++ src/stm32f3hal/dma.c
@@ -14,11 +14,14 @@ void dma_set_transfer_size(DMA_Channel_TypeDef *base, uint16_t num) {
     base->CNDTR = num;
 }
 
-void dma_set_ccr(DMA_Channel_TypeDef *base, uint32_t pl, uint32_t dir,
-                 uint32_t circ, uint32_t minc, uint32_t pinc) {
+void dma_set_ccr(DMA_Channel_TypeDef *base, uint32_t pl, uint32_t msize,
+        uint32_t psize, uint32_t minc, uint32_t pinc, uint32_t circ, uint32_t dir) {
+    // read CCR
     uint32_t reg = base->CCR;
 
     reg &= ~DMA_CCR_PL;     // clear [13:12]
+    reg &= ~DMA_CCR_MSIZE;  // clear [11:10]
+    reg &= ~DMA_CCR_PSIZE;  // clear [9:8]
     reg &= ~DMA_CCR_MINC;   // clear [7]
     reg &= ~DMA_CCR_PINC;   // clear [6]
     reg &= ~DMA_CCR_CIRC;   // clear [5]
@@ -26,6 +29,8 @@ void dma_set_ccr(DMA_Channel_TypeDef *base, uint32_t pl, uint32_t dir,
 
     // modify
     reg |= pl;
+    reg |= msize;
+    reg |= psize;
     reg |= minc;
     reg |= pinc;
     reg |= circ;
@@ -43,7 +48,21 @@ void dma_channel_disable(DMA_Channel_TypeDef *base) {
     base->CCR &= ~DMA_CCR_EN;
 }
 
+void dma_clock_enable(DMA_TypeDef *base) {
+    switch((uint32_t)base) {
+        case (uint32_t)DMA1:
+            RCC->AHBENR |= RCC_AHBENR_DMA1EN;
+            break;
+        case (uint32_t)DMA2:
+            RCC->AHBENR |= RCC_AHBENR_DMA2EN;
+            break;
+    }
+}
+
 void dma_setup(DMA_Config *conf) {
+    // enable the clocks
+    dma_clock_enable(conf->dmabase);
+
     // disable the channel
     dma_channel_disable(conf->base);
 
@@ -51,7 +70,8 @@ void dma_setup(DMA_Config *conf) {
     dma_set_peripheral_addr(conf->base, conf->paddr);
     dma_set_memory_addr(conf->base, conf->maddr);
     dma_set_transfer_size(conf->base, conf->ndata);
-    dma_set_ccr(conf->base, conf->pl, conf->dir, conf->circ, conf->minc, conf->pinc);
+    dma_set_ccr(conf->base, conf->pl, conf->msize, conf->psize, conf->minc,
+            conf->pinc, conf->circ, conf->dir);
 
     // interrupts
     if (conf->tc_int) {
@@ -60,14 +80,14 @@ void dma_setup(DMA_Config *conf) {
     if (conf->ht_int) {
         dma_enable_ht_interrupt(conf->base);
     }
-
-    // enable interrupts on the NVIC
-    if (conf->tc_int || conf->ht_int) {
-        dma_register_irq(conf->irqn, conf->int_pri);
+    if (conf->te_int) {
+        dma_enable_te_interrupt(conf->base);
     }
 
-    // enable the channel
-    dma_channel_enable(conf->base);
+    // enable interrupts on the NVIC
+    if (conf->tc_int || conf->ht_int || conf->te_int) {
+        dma_register_irq(conf->irqn, conf->int_pri);
+    }
 }
 
 /* INTERRUPTS */
@@ -91,4 +111,29 @@ void dma_enable_ht_interrupt(DMA_Channel_TypeDef *base) {
 
 void dma_disable_ht_interrupt(DMA_Channel_TypeDef *base) {
     base->CCR &= ~DMA_CCR_HTIE;
+}
+
+void dma_enable_te_interrupt(DMA_Channel_TypeDef *base) {
+    base->CCR |= DMA_CCR_TEIE;
+}
+
+void dma_disable_te_interrupt(DMA_Channel_TypeDef *base) {
+    base->CCR &= ~DMA_CCR_TEIE;
+}
+
+/* INTERRUPT FLAGS */
+void dma_clearflag_error(DMA_TypeDef *base, uint8_t channel) {
+    base->IFCR |= DMA_IFCR_CTEIF(channel);
+}
+
+void dma_clearflag_ht(DMA_TypeDef *base, uint8_t channel) {
+    base->IFCR |= DMA_IFCR_CHTIF(channel);
+}
+
+void dma_clearflag_tc(DMA_TypeDef *base, uint8_t channel) {
+    base->IFCR |= DMA_IFCR_CTCIF(channel);
+}
+
+void dma_clearflag_global(DMA_TypeDef *base, uint8_t channel) {
+    base->IFCR |= DMA_IFCR_CGIF(channel);
 }
