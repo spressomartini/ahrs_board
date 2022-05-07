f44dbb01e14e46ff786825de389e548f38737d74
Piers Kwan <me@pierskwan.com>
2022-05-07 15:33:42 -0700

    Flashing script using the STM32_Programmer_CLI

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-WHERE=$(dirname $0)
-source $WHERE/openocd_server.sh
-start_oocd
-(echo "init;reset halt; stm32l4x mass_erase 0;flash write_image build/main.bin 0x8000000 bin;reset run"; sleep 1; echo "exit")| telnet localhost 4444
+sudo STM32_Programmer_CLI -c port=usb1 -w ./build/main.bin 0x08000000
+#WHERE=$(dirname $0)
+#source $WHERE/openocd_server.sh
+#start_oocd
+#(echo "init;reset halt; stm32l4x mass_erase 0;flash write_image build/main.bin 0x8000000 bin;reset run"; sleep 1; echo "exit")| telnet localhost 4444
 
