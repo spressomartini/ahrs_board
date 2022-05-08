aedf393260b528ef9bb894acea66644d01244987
Piers Kwan <me@pierskwan.com>
2022-05-07 17:19:00 -0700

    Not sure what happened here with the ^M, replaced basically the whole file

--- src/main.c
+++ src/main.c
@@ -8,23 +8,8 @@
 #define TSTRING1_LEN    (28 * 16)
 #define TSTRING2_LEN    (14)
 
-const char teststring1[] = "" 
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r"
-"abcdefghijklmnopqrstuvwxyz\n\r";
+const char teststring1[] = ""
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\r";
 
 const char teststring2[] = "Hello World!\n\r";
 
@@ -44,7 +29,7 @@ int main(void){
     __set_PRIMASK(primask); /* end interrupt-sensitive init */
 
     uart1_queue_transmit(teststring1, TSTRING1_LEN);
-    
+
     while (1) {
         led_toggle(GREEN_LED_PIN);
         uart1_queue_transmit(teststring1, TSTRING1_LEN);
