9ec99d02ea1698ffa8a9ce7868c8bde87c9eca46
Piers Kwan <me@pierskwan.com>
2022-07-18 22:31:24 -0700

    Functions to use the bmx055

--- include/devices/bmx055.h
+++ include/devices/bmx055.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <stm32f302xc.h>
 
 #define BMX055_POW_PIN  (5u)
+#define BMX055_ACCEL_I2C_ADDR       (0x18u)
+#define BMX055_GYRO_I2C_ADDR        (0x68u)
+#define BMX055_MAGNET_I2C_ADDR      (0x10u)
+
+#define BMX055_ACCEL_BGW_CHIPID_ADDR    (0x00)
+#define BMX055_GYRO_BGW_CHIPID_ADDR     (0x00)
+#define BMX055_MAGNET_BGW_CHIPID_ADDR   (0x00)
 
 
-void bmx055_pow_setup(void);
-void bmx055_on();
-void bmx055_off();
+void bmx055_setup(void);
+void bmx055_on(void);
+void bmx055_off(void);
+void bmx055_read_accel_whoami(uint8_t *buf);
+void bmx055_read_gyro_whoami(uint8_t *buf);
+void bmx055_read_magnet_whoami(uint8_t *buf);
--- src/devices/bmx055.c
+++ src/devices/bmx055.c
@@ -1,5 +1,6 @@
 #include "devices/bmx055.h"
 #include "stm32f3hal/gpio.h"
+#include "stm32f3hal/i2c.h"
 
 static GPIO_PIN_Config bmx055_pow_config = {
     .base = GPIOA,
@@ -10,15 +11,53 @@ static GPIO_PIN_Config bmx055_pow_config = {
     .ospeed = GPIO_OSPEEDR_LOW,
 };
 
-void bmx055_pow_setup(void) {
+static GPIO_PIN_Config i2c1_scl_pin_config = {
+    .base = GPIOB,
+    .pin = 6,
+    .mode = GPIO_MODER_ALT,
+    .pull = GPIO_PUPDR_NOPULL,
+    .otype = GPIO_OTYPER_PUSHPULL,
+    .ospeed = GPIO_OSPEEDR_HIGH,
+    .alt = 4,
+};
+
+static GPIO_PIN_Config i2c1_sda_pin_config = {
+    .base = GPIOB,
+    .pin = 7,
+    .mode = GPIO_MODER_ALT,
+    .pull = GPIO_PUPDR_NOPULL,
+    .otype = GPIO_OTYPER_PUSHPULL,
+    .ospeed = GPIO_OSPEEDR_HIGH,
+    .alt = 4,
+};
+
+void bmx055_setup(void) {
     gpio_pin_setup(&bmx055_pow_config);
+    gpio_pin_setup(&i2c1_scl_pin_config);
+    gpio_pin_setup(&i2c1_sda_pin_config);
+    i2c_master_setup(&i2c_100khz_config);
 }
 
-void bmx055_on() {
+void bmx055_on(void) {
     gpio_write_pin(GPIOA, BMX055_POW_PIN, GPIO_PIN_RESET);
+    for (volatile int i = 0; i < 1000000; i++);
 }
 
-void bmx055_off() {
+void bmx055_off(void) {
     gpio_write_pin(GPIOA, BMX055_POW_PIN, GPIO_PIN_SET);
 }
 
+void bmx055_read_accel_whoami(uint8_t *buf) {
+    uint8_t reg[] = {BMX055_ACCEL_BGW_CHIPID_ADDR};
+    i2c_master_transfer7(I2C1, BMX055_ACCEL_I2C_ADDR, reg, 1, buf, 1);
+}
+
+void bmx055_read_gyro_whoami(uint8_t *buf) {
+    uint8_t reg[] = {BMX055_GYRO_BGW_CHIPID_ADDR};
+    i2c_master_transfer7(I2C1, BMX055_GYRO_I2C_ADDR, reg, 1, buf, 1);
+}
+
+void bmx055_read_magnet_whoami(uint8_t *buf) {
+    uint8_t reg[] = {BMX055_MAGNET_BGW_CHIPID_ADDR};
+    i2c_master_transfer7(I2C1, BMX055_MAGNET_I2C_ADDR, reg, 1, buf, 1);
+}
