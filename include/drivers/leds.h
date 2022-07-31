c679bea12b8948413740ca10f979672f68607a96
Piers Kwan <me@pierskwan.com>
2022-07-31 15:44:48 -0700

    Forgot to remove defines from leds.h

--- include/drivers/leds.h
+++ include/drivers/leds.h
@@ -1,9 +1,6 @@
 #pragma once
 #include "stm32f3hal/gpio.h"
 
-#define GREEN_LED_PIN   (0u)
-#define RED_LED_PIN     (1u)
-
 void led_setup(void);
 void led_on(uint32_t led);
 void led_off(uint32_t led);
