f62f0da84a99d39ea7e061b6bdfa82820397944f
Piers Kwan <me@pierskwan.com>
2023-02-23 20:41:54 -0800

    Cleaned up main a bit. Finally moved the stuff out of the systick interrupt! Using flags to trigger functionality in the runloop now

--- src/main.c
+++ src/main.c
@@ -11,20 +11,23 @@
 #include "algos/quaternion.h"
 #include "algos/madgwick.h"
 #include "cm4/systick.h"
+#define VERBOSE
 
-#define TSTRING1_LEN    (28 * 16)
-
-const char teststring1[] = ""
-"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\r";
-
-const char newline[] = "\n\r";
+#define INIT_MSG_LEN    (49)
+#define NEWLINE_LEN     (2)
+const char INIT_MSG_BUF[] = "Successfully initialized peripherals and devices!";
+const char NEWLINE_BUF[] = "\n\r";
 
 BMX055_AccelData_t accel_data;
 BMX055_GyroData_t gyro_data;
 BMX055_MagnetData_t magnet_data;
-bool fresh_data = false;
 
-int main(void){
+// systick flags
+volatile bool led_toggle_flag = false;
+volatile bool flush_uart_flag = false;
+volatile bool read_data_flag = false;
+
+static inline void main_init(void) {
     uint32_t primask = __get_PRIMASK();
     __disable_irq();        /* begin interrupt-sensitive init */
 
@@ -42,12 +45,45 @@ int main(void){
     bmx055_on();
 
     __set_PRIMASK(primask); /* end interrupt-sensitive init */
+}
 
-    uart1_queue_transmit(teststring1, TSTRING1_LEN);
-
+static inline void bmx_diagnostics(void) {
     uint8_t whoami;
     uint16_t hex;
     char whoami_str[4] = "0x??";
+
+    // toggle on the green led
+    led_toggle(GREEN_LED_PIN);
+
+    // try contacting accelerometer
+    bmx055_read_accel_whoami(&whoami);
+    hex = bin2hex(whoami);
+    whoami_str[2] = (hex >> 8) & 0xFFu;
+    whoami_str[3] = (hex & 0xFFu);
+    uart1_queue_transmit(whoami_str, 4);
+    uart1_queue_transmit(NEWLINE_BUF, NEWLINE_LEN);
+
+    // try contacting gyroscope
+    bmx055_read_gyro_whoami(&whoami);
+    hex = bin2hex(whoami);
+    whoami_str[2] = (hex >> 8) & 0xFFu;
+    whoami_str[3] = (hex & 0xFFu);
+    uart1_queue_transmit(whoami_str, 4);
+    uart1_queue_transmit(NEWLINE_BUF, NEWLINE_LEN);
+
+    // try contacting magnetometer
+    bmx055_read_magnet_whoami(&whoami);
+    hex = bin2hex(whoami);
+    whoami_str[2] = (hex >> 8) & 0xFFu;
+    whoami_str[3] = (hex & 0xFFu);
+    uart1_queue_transmit(whoami_str, 4);
+    uart1_queue_transmit(NEWLINE_BUF, NEWLINE_LEN);
+
+    // toggle off green led
+    led_toggle(GREEN_LED_PIN);
+}
+
+int main(void) {
     euler_angle eu_debug;
 
     // mpack
@@ -55,37 +91,43 @@ int main(void){
     size_t used;
     mpack_writer_t writer;
 
-    led_toggle(GREEN_LED_PIN);
-    bmx055_read_accel_whoami(&whoami);
-    hex = bin2hex(whoami);
-    whoami_str[2] = (hex >> 8) & 0xFFu;
-    whoami_str[3] = (hex & 0xFFu);
-    uart1_queue_transmit(whoami_str, 4);
-    uart1_queue_transmit(newline, 2);
+    // bring up the peripherals and devices
+    main_init();
 
-    bmx055_read_gyro_whoami(&whoami);
-    hex = bin2hex(whoami);
-    whoami_str[2] = (hex >> 8) & 0xFFu;
-    whoami_str[3] = (hex & 0xFFu);
-    uart1_queue_transmit(whoami_str, 4);
-    uart1_queue_transmit(newline, 2);
+#ifdef VERBOSE
+    // transmit a startup message over uart
+    uart1_queue_transmit(INIT_MSG_BUF, INIT_MSG_LEN);
+    uart1_queue_transmit(NEWLINE_BUF, NEWLINE_LEN);
 
-    bmx055_read_magnet_whoami(&whoami);
-    hex = bin2hex(whoami);
-    whoami_str[2] = (hex >> 8) & 0xFFu;
-    whoami_str[3] = (hex & 0xFFu);
-    uart1_queue_transmit(whoami_str, 4);
-    uart1_queue_transmit(newline, 2);
+    // run bmx055 diagnostics
+    bmx_diagnostics();
+#endif
 
+    // XXX figure out why you put this here and not with the main init
     bmx055_magnet_setup();
 
     while (1) {
-        if (fresh_data) {
+        if (led_toggle_flag) {
+            led_toggle(RED_LED_PIN);
+            led_toggle_flag = false;
+        }
+
+        if (flush_uart_flag) {
+            uart1_dma_fsm();
+            flush_uart_flag = false;
+        }
+
+        if (read_data_flag) {
+            bmx055_accel_burst_read_data(&accel_data);
+            bmx055_gyro_burst_read_data(&gyro_data);
+            bmx055_magnet_burst_read_data(&magnet_data);
+            read_data_flag = false;
+
+            // filter
             imu_filter(accel_data.x, accel_data.y, accel_data.z, gyro_data.x, gyro_data.y, gyro_data.z);
             eu_debug = q_toeuler(&q_est);
-            //pack_aproto(&aproto_packet, accel_data.x, accel_data.y, accel_data.z, gyro_data.x, gyro_data.y, gyro_data.z,
-            //            magnet_data.x, magnet_data.y, magnet_data.z, q_est.a, q_est.b, q_est.c, q_est.d,
-            //            eu_debug.roll, eu_debug.pitch, eu_debug.yaw);
+
+            // messagepack
             mpack_writer_init(&writer, mpack_buf, 64);
             mpack_start_map(&writer, 1);
             mpack_write_cstr(&writer, "ax");
@@ -98,7 +140,6 @@ int main(void){
             used = mpack_writer_buffer_used(&writer);
             mpack_writer_destroy(&writer);
             uart1_queue_transmit(mpack_buf, used);
-            fresh_data = false;
         }
     }
 
@@ -106,39 +147,13 @@ int main(void){
     for(volatile int i = 0;;i++) {
         i;
     }
-    // encode to memory buffer
-//char* data;
-//size_t size;
-//mpack_writer_t writer;
-//mpack_writer_init_growable(&writer, &data, &size);
-// 
-//// write the example on the msgpack homepage
-//mpack_build_map(&writer);
-//mpack_write_cstr(&writer, "compact");
-//mpack_write_bool(&writer, true);
-//mpack_write_cstr(&writer, "schema");
-//mpack_write_uint(&writer, 0);
-//mpack_complete_map(&writer);
-// 
-//// finish writing
-//if (mpack_writer_destroy(&writer) != mpack_ok) {
-//    fprintf(stderr, "An error occurred encoding the data!\n");
-//    return;
-//}
-// 
-//// use the data
-//do_something_with_data(data, size);
-//free(data);
 }
 
 void SysTick_Handler(void) {
-    // put periodic functions here
-    led_toggle(RED_LED_PIN);
-    uart1_dma_fsm();
-    bmx055_accel_burst_read_data(&accel_data);
-    bmx055_gyro_burst_read_data(&gyro_data);
-    bmx055_magnet_burst_read_data(&magnet_data);
-    fresh_data = true;
+    // flags to signal functionality in runloop
+    led_toggle_flag = true;
+    flush_uart_flag = true;
+    read_data_flag = true;
 }
 
 void HardFault_Handler(void) {
