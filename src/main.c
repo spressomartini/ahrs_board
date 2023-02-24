4c89a239e9f99d8c7ab4aea032f118b5291688df
Piers Kwan <me@pierskwan.com>
2023-02-23 19:32:21 -0800

    Using messagepack instead of naive protocol. WIP, this commit is very likely broken. Also, FTDI issues, the tx to linux will not work as is

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "stm32f3hal/rcc.h"
 #include "utils/bin2hex.h"
 #include "utils/aproto.h"
+#include "utils/mpack.h"
 #include "drivers/leds.h"
 #include "drivers/uart.h"
 #include "devices/bmx055.h"
@@ -48,7 +49,11 @@ int main(void){
     uint16_t hex;
     char whoami_str[4] = "0x??";
     euler_angle eu_debug;
-    aproto_t aproto_packet;
+
+    // mpack
+    char mpack_buf[64];
+    size_t used;
+    mpack_writer_t writer;
 
     led_toggle(GREEN_LED_PIN);
     bmx055_read_accel_whoami(&whoami);
@@ -78,19 +83,52 @@ int main(void){
         if (fresh_data) {
             imu_filter(accel_data.x, accel_data.y, accel_data.z, gyro_data.x, gyro_data.y, gyro_data.z);
             eu_debug = q_toeuler(&q_est);
-            pack_aproto(&aproto_packet, accel_data.x, accel_data.y, accel_data.z, gyro_data.x, gyro_data.y, gyro_data.z,
-                        magnet_data.x, magnet_data.y, magnet_data.z, q_est.a, q_est.b, q_est.c, q_est.d,
-                        eu_debug.roll, eu_debug.pitch, eu_debug.yaw);
-            uart1_queue_transmit((char *)&aproto_packet, APROTO_PACKET_SIZE);
+            //pack_aproto(&aproto_packet, accel_data.x, accel_data.y, accel_data.z, gyro_data.x, gyro_data.y, gyro_data.z,
+            //            magnet_data.x, magnet_data.y, magnet_data.z, q_est.a, q_est.b, q_est.c, q_est.d,
+            //            eu_debug.roll, eu_debug.pitch, eu_debug.yaw);
+            mpack_writer_init(&writer, mpack_buf, 64);
+            mpack_start_map(&writer, 1);
+            mpack_write_cstr(&writer, "ax");
+            mpack_write_u16(&writer, accel_data.x);
+            mpack_write_cstr(&writer, "ay");
+            mpack_write_u16(&writer, accel_data.y);
+            mpack_write_cstr(&writer, "az");
+            mpack_write_u16(&writer, accel_data.z);
+            mpack_finish_map(&writer);
+            used = mpack_writer_buffer_used(&writer);
+            mpack_writer_destroy(&writer);
+            uart1_queue_transmit(mpack_buf, used);
             fresh_data = false;
         }
-        //for(volatile int i = 0; i < 1000000; i++);
     }
 
     // no touchy
     for(volatile int i = 0;;i++) {
         i;
     }
+    // encode to memory buffer
+//char* data;
+//size_t size;
+//mpack_writer_t writer;
+//mpack_writer_init_growable(&writer, &data, &size);
+// 
+//// write the example on the msgpack homepage
+//mpack_build_map(&writer);
+//mpack_write_cstr(&writer, "compact");
+//mpack_write_bool(&writer, true);
+//mpack_write_cstr(&writer, "schema");
+//mpack_write_uint(&writer, 0);
+//mpack_complete_map(&writer);
+// 
+//// finish writing
+//if (mpack_writer_destroy(&writer) != mpack_ok) {
+//    fprintf(stderr, "An error occurred encoding the data!\n");
+//    return;
+//}
+// 
+//// use the data
+//do_something_with_data(data, size);
+//free(data);
 }
 
 void SysTick_Handler(void) {
